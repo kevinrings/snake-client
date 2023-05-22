5f08796ba1c5513c78f1cd3786a6d8ce4671d72c
kevinrings <wnkevin@gmail.com>
2023-05-22 02:29:32 -0700

    pass the object returned from connect into the setInput function

--- play.js
+++ play.js
@@ -1,29 +1,7 @@
-const net = require("net");
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log(`Successfully connected to game server`);
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-
-
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-module.exports = { connect };
+const conn = connect();
+setupInput(conn);
