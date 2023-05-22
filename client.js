5a34653a0efc53eff291be9d440d871e6a302dc9
kevinrings <wnkevin@gmail.com>
2023-05-22 01:55:36 -0700

    Stdin Setup for user input

--- client.js
+++ client.js
@@ -26,8 +26,6 @@ const connect = function () {
   return conn;
 };
 
-// setup interface to handle user input from stdin
-
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
